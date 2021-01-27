dfbdd9cbff712acbfeda55a6f90ce38f09f3c87f
nikolaskorakidis <korakidisn@gmail.com>
2021-01-27 16:59:58 +0100

    GET Request

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Http-Requests-with-XMLHttpRequest
+
+A workshop about Sending JavaScript Http Requests with XMLHttpRequest, i amleaving comments and make sure to check my commits if you wanna follow my pathing and understand tha basic structure of it.
--- xhr.js
+++ xhr.js
@@ -1,7 +1,23 @@
 const getBtn = document.getElementById("get-btn");
 const postBtn = document.getElementById("post-btn");
 
-const getData = () => {};
+const getData = () => {
+  // We declaire our xhr request adn save it to a const
+  const xhr = new XMLHttpRequest();
+  //   we send a req to get the data from our api liek we would normally do with our first argument to be the kind of req we need and second will be our api
+  xhr.open("GET", "https://reqres.in/api/users");
+  //   When we do that we dont get a JSOn file so we need to parse it into JSON. One way is to normally parse our data but we will go with the second option which is to declaire our response type.
+  xhr.responseType = "json";
+  //   Time to add our listener and since not all browsers use .eventLister() we will do an on load function.
+  xhr.onload = () => {
+    const data = xhr.response;
+    console.log(data);
+  };
+  //   Last spet is to call our send function on our xhr
+  xhr.send();
+
+  //   Now if we inspect our website and check our action we will see our console.log
+};
 
 const sendData = () => {};
 
